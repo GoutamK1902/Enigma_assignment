e5addc6a30f00e77f2de3c06dc2dbfdc5aae6d3c
GoutamK1902 <goutamkhowal321@gmail.com>
2024-09-26 00:06:58 +0530

    first commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,196 @@
+"use client";
+
+// import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import Link from "next/link";
+import { useRef, useState } from "react";
+import { LuCircleDotDashed } from "react-icons/lu";
+import mediaQueries from "../utils/mediaQueries.mjs";
+
+const Nav = () => {
+  const navBodyRef = useRef<HTMLDivElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const openNav = () => {
+    const mm = gsap.matchMedia();
+
+    mediaQueries.forEach(
+      ({
+        mqCondtion,
+        navHeight,
+        navWidth,
+        navTranslateX,
+        navLinkTranslateX,
+        navLinkTranslateY,
+        linkFontSize,
+        linkHeight,
+      }) => {
+        mm.add(`${mqCondtion}`, () => {
+          const tl = gsap.timeline();
+          gsap.to(overlayRef.current, {
+            opacity: 1,
+            height: "100vh",
+            width: "100vw",
+            duration: 0.0002,
+            delay: 0,
+            position: "fixed",
+            overflow: "visible",
+          });
+          gsap.set(navBodyRef.current, {
+            translateX: navTranslateX,
+            overflow: "visible",
+          });
+          gsap.set(".navLink", {
+            translateX: navLinkTranslateX,
+            translateY: navLinkTranslateY,
+            // overflow: "visible",
+            fontSize: linkFontSize,
+          });
+          gsap.set(".lineHeight", {
+            height: linkHeight,
+          });
+          tl.to(navBodyRef.current, {
+            height: navHeight, //change
+            width: navWidth, //change
+            duration: 0.9,
+            delay: 0,
+            ease: "none",
+          });
+          tl.fromTo(
+            ".navLink",
+            {
+              top: "400%",
+              left: "250%",
+              duration: 0.8,
+              opacity: 0.2,
+              ease: "power2.inOut",
+              stagger: 0.2,
+              delay: 0.2,
+            },
+            {
+              top: "0",
+              left: "0",
+              duration: 0.8,
+              opacity: 1,
+              ease: "power2.inOut",
+              stagger: 0.2,
+              delay: 0.2,
+            }
+          );
+        });
+      }
+    );
+    setIsOpen(true);
+  };
+  const closeNav = () => {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      ".navLink",
+      {
+        top: "0",
+        left: "0",
+        duration: 0.8,
+        opacity: 0.2,
+        ease: "power2.inOut",
+        stagger: 0.2,
+        delay: 0.2,
+      },
+      {
+        top: "400%",
+        left: "250%",
+        duration: 0.8,
+        opacity: 0.2,
+        ease: "power2.inOut",
+        stagger: 0.2,
+        delay: 0,
+      }
+    );
+    gsap.to(overlayRef.current, {
+      opacity: 0,
+      height: 0,
+      width: 0,
+      duration: 1,
+      delay: 0,
+    });
+    tl.to(navBodyRef.current, {
+      height: 0,
+      width: 0,
+      duration: 1,
+      delay: 0,
+    });
+    setIsOpen(false);
+  };
+  return (
+    <>
+      <div
+        onClick={() => {
+          closeNav();
+        }}
+        ref={overlayRef}
+        className="top-0 left-0
+            h-0 w-0
+            opacity-0
+            overflow-hidden
+            bg-primary/20
+        z-[1000]
+        "
+      />
+      <button
+        className="fixed bottom-2 left-2 size-10 group z-[1001]"
+        onClick={() => {
+          if (isOpen) {
+            closeNav();
+          } else {
+            openNav();
+          }
+        }}
+      >
+        <LuCircleDotDashed
+          className={`size-full ${isOpen ? "text-orange-400" : "text-accent"}`}
+        />
+      </button>
+      <div
+        ref={navBodyRef}
+        className="
+                fixed
+                -bottom-[100%]
+                
+                -left-[50%]
+        
+                bg-accent
+                rounded-full
+                z-[1000]
+                text-primary
+                "
+      >
+        <div
+          className="h-80 w-[45vw]  translate-x-[115%] translate-y-[95%]
+                font-bebas text-8xl
+                uppercase
+                
+                "
+        >
+          <div>
+            <div className="lineHeight relative  overflow-visible ">
+              <span className="navLink absolute top-0 left-0">
+                <Link href={""}>HOME</Link>
+              </span>
+            </div>
+            <div className="lineHeight relative  overflow-visible ">
+              <span className="navLink absolute top-0 left-0">
+                <Link href={""}>Our team</Link>
+              </span>
+            </div>
+            <div className="lineHeight relative  overflow-visible ">
+              <span className="navLink absolute top-0 left-0">
+                <Link href={""}>Reach us</Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
---
+++ src/utils/mediaQueries.mjs
@@ -0,0 +1,45 @@
+const mediaQueries = [
+  {
+    mqCondtion: "(min-width:320px)",
+    navHeight: "160vh",
+    navWidth: "185vh",
+    navTranslateX: "-80vh",
+    navLinkTranslateX: "80vh",
+    navLinkTranslateY: "-28vh",
+    linkFontSize: "2rem",
+    linkHeight: "2rem",
+  },
+  {
+    mqCondtion: "(min-width:360px)",
+    navHeight: "160vh",
+    navWidth: "185vh",
+    navTranslateX: "-97vh",
+    navLinkTranslateX: "98vh",
+    navLinkTranslateY: "-18vh",
+    linkFontSize: "3rem",
+    linkHeight: "4rem",
+  },
+  {
+    mqCondtion: "(min-width:700px)",
+    navHeight: "160vh",
+    navWidth: "200vh",
+    navTranslateX: "-97vh",
+    navLinkTranslateX: "98vh",
+    navLinkTranslateY: "-3vh",
+    linkFontSize: "5.5rem",
+    linkHeight: "6rem",
+  },
+  {
+    mqCondtion: "(min-width:900px)",
+    navHeight: "190vh",
+    navWidth: "250vh",
+    navTranslateX: "-50vh",
+    navLinkTranslateX: "50vh",
+    navLinkTranslateY: "-12vh",
+    linkFontSize: "6rem",
+    linkHeight: "6.8rem",
+  },
+];
+
+export default mediaQueries;
+// done
